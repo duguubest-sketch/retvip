bacf0c58d05fe692a95d4ea2691757aa32df1b34
duguubest-sketch <duguubest@gmail.com>
2026-03-29 11:12:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const http = require("http")
 const socket = require("websocket").server
-const server = http.createServer(() => {
+
+// 1. Render dynamic port assign karta hai, isliye process.env.PORT use karna zaroori hai
+const PORT = process.env.PORT || 3000; 
+
+// 2. HTTP Server create kiya taaki Render ko "Live" status mil sake
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Signaling Server is Live and Running!");
 })
 
-server.listen(3000, () => {
-
-})
+// 3. Server ko listen karwaya (0.0.0.0 se ye bahar ki duniya ke liye open ho jata hai)
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
 
 const users = []
 
@@ -19,8 +27,8 @@ const Types = {
     EndCall: "EndCall",
 }
 
+// 4. WebSocket setup jo hamare HTTP server ka use karega
 const webSocket = new socket({httpServer: server})
-
 
 webSocket.on('request', (req) => {
     const connection = req.accept();
@@ -30,16 +38,18 @@ webSocket.on('request', (req) => {
             const data = JSON.parse(message.utf8Data);
             const currentUser = findUser(data.username)
             const userToReceive = findUser(data.target)
-            console.log(data)
+            console.log("Received data:", data.type, "from:", data.username);
 
             switch (data.type) {
                 case Types.SignIn:
                     if (currentUser) {
-                        return
+                        // Agar user pehle se hai toh purana connection update kar sakte hain
+                        currentUser.conn = connection;
+                        return;
                     }
-
                     users.push({username: data.username, conn: connection, password: data.data})
                     break
+                
                 case Types.StartStreaming :
                     if (userToReceive) {
                             sendToConnection(userToReceive.conn, {
@@ -49,6 +59,7 @@ webSocket.on('request', (req) => {
                             })
                     }
                     break
+
                 case Types.Offer :
                     if (userToReceive) {
                         sendToConnection(userToReceive.conn, {
@@ -56,6 +67,7 @@ webSocket.on('request', (req) => {
                         })
                     }
                     break
+
                 case Types.Answer :
                     if (userToReceive) {
                         sendToConnection(userToReceive.conn, {
@@ -63,6 +75,7 @@ webSocket.on('request', (req) => {
                         })
                     }
                     break
+
                 case Types.IceCandidates:
                     if (userToReceive) {
                         sendToConnection(userToReceive.conn, {
@@ -70,6 +83,7 @@ webSocket.on('request', (req) => {
                         })
                     }
                     break
+
                 case Types.EndCall:
                     if (userToReceive) {
                         sendToConnection(userToReceive.conn, {
@@ -79,26 +93,28 @@ webSocket.on('request', (req) => {
                     break
             }
         } catch (e) {
-            console.log(e.message)
+            console.log("Error handling message:", e.message)
         }
-
     });
+
     connection.on('close', () => {
-        users.forEach(user => {
-            if (user.conn === connection) {
-                users.splice(users.indexOf(user), 1)
+        // User disconnect hone par list se hatana
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].conn === connection) {
+                console.log("User disconnected:", users[i].username);
+                users.splice(i, 1);
+                break;
             }
-        })
+        }
     })
 });
 
-
 const sendToConnection = (connection, message) => {
-    connection.send(JSON.stringify(message))
+    if (connection) {
+        connection.send(JSON.stringify(message))
+    }
 }
 
 const findUser = username => {
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username) return users[i]
-    }
+    return users.find(user => user.username === username);
 }
